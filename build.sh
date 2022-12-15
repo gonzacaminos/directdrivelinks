568a3e44e125ae713c19d96da71a2cabc82ae238
gonzacaminos <5827300+gonzalobonini@users.noreply.github.com>
2022-12-15 15:46:53 -0300

    Added build command

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Build script for serving static content instead of php files
+DEST="./public_html"
+html=".html"
+
+# Create destination folder
+mkdir -p "$DEST/"
+cp -r css img js "$DEST/"
+# Execute all php files and save them as html
+for f in *.php; 
+do
+    php $f | sed 's:\(<a.*href=".*\)\.php\(".*</a>\):\1\.html\2:g' > "$DEST/${f/.php/$html}";
+    echo "Processing $f into ${f/.php/$html}..";
+done
+
+
+echo "Process complete." ;
